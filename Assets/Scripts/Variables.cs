fbc1133c976b9c1968424cb75903ae04694e6de6
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-17 13:17:16 -0500

    Added lab2

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -7,16 +7,38 @@ public class Variables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Surrounding text in double-quotes will log the text,
-        // whereas logging a variable will output its value
         int integer = 0;
-        Debug.Log(integer);     // outputs 0
-        Debug.Log("integer");   // outputs integer
+        Debug.Log(integer);     // logs the value 0
+        Debug.Log("integer");   // logs the word integer
+
+        // We can add, subtract, multiply and divide whole numbers
+        int a = 1;
+        int b = 2;
+        Debug.Log(a + b);
+        Debug.Log(a - b);
+        Debug.Log(a * b);
+        Debug.Log(a / b);
+
+        // Integers round *down* to the nearest whole number "truncation"
+        int n = (int)0.99999;
+        Debug.Log(n);
+
+        // We can also add, subtract, multiply and divide decimal numbers!
+        float c = 1.0f;
+        float d = 2.0f;
+        Debug.Log(c + d);
+        Debug.Log(c - d);
+        Debug.Log(c * d);
+        Debug.Log(c / d);
     }
+
+    // The value in the inspector will override whatever value we have here
+    // (So x will be -5 instead of 10000000)
+    [SerializeField] float x = 10000000.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(x, 0.0f, 0.0f);
     }
 }
